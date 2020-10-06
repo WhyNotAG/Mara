f097daf4940a3f04b2483571fc652f0da5c30dd6
WhyNotAG <golubkin1998@mail.ru>
2020-10-06 16:46:49 +0300

    add player and camera movement

---
+++ Mara/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+     
+    public int dumping = 15; //слгаживание при перемещении
+    public Vector2 offset = new Vector2(3f, 1f); //смещение камеры относительно персонаж
+    private Transform player;
+    // Start is called before the first frame update 
+
+    void Start() {
+        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+        FindPlayer(PlayerController.facingRight);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (player) {
+
+            Vector3 target;
+            if (!PlayerController.facingRight) {
+                target = new Vector3(player.position.x - offset.x * 3, player.position.y + offset.y, transform.position.z);
+            }
+            else {
+                target = new Vector3(player.position.x + offset.x * 3, player.position.y + offset.y, transform.position.z);
+            }
+
+            Vector3 currentPosition = Vector3.Lerp(transform.position, target, dumping * Time.deltaTime);
+            transform.position = currentPosition;
+        }
+
+    }
+
+    public void FindPlayer(bool isRight) {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    
+        if (!PlayerController.facingRight) {
+            transform.position = new Vector3(player.position.x - offset.x * 3, player.position.y + offset.y, transform.position.z);
+        }
+        else {
+            transform.position = new Vector3(player.position.x + offset.x * 3, player.position.y + offset.y, transform.position.z);
+        }
+    }
+}
---
+++ Mara/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public float speed = 20.0f;
+    public static bool facingRight = true;
+    private Rigidbody2D rb;
+    private Animator playerAnimator;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float moveX = Input.GetAxis("Horizontal");
+
+        if (moveX == 0) playerAnimator.Play("Stay");
+        else Walk(moveX, facingRight);
+
+        if (moveX < 0 && facingRight || moveX > 0 && !facingRight) Flip();
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+
+    void Walk(float move, bool isRight) {
+        if (isRight) rb.transform.Translate(move * speed * Time.deltaTime, 0, 0);
+        else rb.transform.Translate(-move * speed * Time.deltaTime, 0, 0);
+
+        playerAnimator.StopPlayback();
+        playerAnimator.Play("Walk");
+    }
+}
